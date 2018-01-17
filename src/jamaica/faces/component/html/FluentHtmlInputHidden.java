62c4263f82fa33b5ca9fc0de79e0f516419390e3
Roger Keays <roger.keays@ninthavenue.com.au>
2018-01-17 22:20:17 +0700

    move novdl code

--- src/jamaica/faces/component/html/FluentHtmlInputHidden.java
+++
@@ -1,9 +0,0 @@
-package jamaica.faces.component.html;
-
-import jamaica.faces.component.FluentUIInput;
-
-public interface FluentHtmlInputHidden<T extends FluentHtmlInputHidden>
-            extends FluentUIInput<T> {
-
-}
-
